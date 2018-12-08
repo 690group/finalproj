3580d0b036fd22460ed0a243263bc6200e32a75e
Jasmine Chen <milktea@Jasmines-iMac.local>
2018-12-07 23:34:28 -0800

    start

---
+++ finalprojTests/finalprojTests.swift
@@ -0,0 +1,34 @@
+//
+//  finalprojTests.swift
+//  finalprojTests
+//
+//  Created by Jasmine Chen on 12/7/18.
+//  Copyright © 2018 Jasmine Chen. All rights reserved.
+//
+
+import XCTest
+@testable import finalproj
+
+class finalprojTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
