38529c0097d0ea001c1b9a01e9362c548dc75dfc
stephaniesantana <stephycsantana@gmail.com>
2018-12-10 16:49:14 -0800

    Updated Calculator Storyboard and outlets hooked up

--- finalproj/Calculator.swift
+++ finalproj/Calculator.swift
@@ -2,7 +2,30 @@ import UIKit
 
 class Calculator: UIViewController {
 
+    @IBOutlet weak var ageInput: UITextField!
+    @IBOutlet weak var male: UIButton!
+    @IBOutlet weak var female: UIButton!
+    
+    @IBOutlet weak var feet: UITextField!
+    
+    @IBOutlet weak var inches: UITextField!
+    
+    @IBOutlet weak var fatLoss: UIButton!
+    
+    @IBOutlet weak var maintenance: UIButton!
+    
+    @IBOutlet weak var gainMuscle: UIButton!
+    
+    @IBOutlet weak var sedentaryLevel: UIButton!
+    
+    @IBOutlet weak var moderateLevel: UIButton!
+    @IBOutlet weak var activeLevel: UIButton!
+    
+    @IBOutlet weak var calculateValues: UIButton!
+    
     override func viewDidLoad() {
+        
+   
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
