9978e4d4905689608c6690b88699d2bba8edd9a7
stephaniesantana <stephycsantana@gmail.com>
2018-12-14 03:22:05 -0800

    Functional Activity loader with WKWebView is working properly

--- Controller/websiteViewController.swift
+++ Controller/websiteViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 import WebKit
 
-class websiteViewController: UIViewController {
+class websiteViewController: UIViewController, WKNavigationDelegate{
     
     @IBOutlet weak var web: WKWebView!
     
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //delegate back to itself for navigation delgate
+        web.navigationDelegate = self
         
         //this will load a page for bodybuilding.com about macros
         let myurl = URL(string: "http://www.bodybuilding.com/content/from-here-to-macros-4-steps-to-better-nutrition.html")
@@ -23,6 +29,15 @@ class websiteViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    //when webview is loading, start activity loader
+    func webView(_ web:WKWebView, didStartLoad Navigation: WKNavigation!){
+        activity.startAnimating()
+    }
     
+    //when webview finished loading, remove activity loader
+  func webView(_ web:WKWebView, didFinish Navigation: WKNavigation!){
+        activity.stopAnimating()
+        activity.hidesWhenStopped = true
+    }
 }
 
